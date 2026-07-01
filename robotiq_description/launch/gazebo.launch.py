45e2e8643b3acfa4c303059028d82e8b9c479f40
maya-leblanc <stemgirl25@gmail.com>
2026-07-01 19:28:32 -0400

    Fix mimic joints for Gazebo compatibility

---
+++ robotiq_description/launch/gazebo.launch.py
@@ -0,0 +1,60 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+
+    pkg = get_package_share_directory('robotiq_description')
+
+    xacro_file = os.path.join(pkg, 'urdf', 'robotiq_2f_85_gripper.urdf.xacro')
+    robot_description = xacro.process_file(xacro_file, mappings={
+        'sim_gazebo': 'true',
+        'use_fake_hardware': 'true'
+    }).toxml()
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    gazebo = ExecuteProcess(
+        cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+        output='screen'
+    )
+
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic', 'robot_description',
+                   '-entity', 'robotiq_gripper',
+                   '-x', '0',
+                   '-y', '0',
+                   '-z', '0.5'],
+        output='screen'
+    )
+
+    joint_state_broadcaster = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        robot_state_publisher,
+        gazebo,
+        spawn_entity,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[joint_state_broadcaster]
+            )
+        )
+    ])
